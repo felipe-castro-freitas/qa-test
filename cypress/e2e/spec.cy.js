1135ff4f4d988e3b4faaec532f897f49ef7f2445
Felipe Castro de Freitas <felipecastrofreitas@gmail.com>
2025-04-24 12:51:36 -0300

    removed example

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe("template spec", () => {
-  beforeEach(() => {
-    cy.visit("https://demoqa.com/");
-    cy.waitForPa;
-  });
-  it("passes", () => {
-    cy.visit("https://demoqa.com/");
-  });
-});
